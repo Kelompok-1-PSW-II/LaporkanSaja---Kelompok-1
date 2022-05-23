925d20be2d53a2794b2d1120782a494e003dc247
Marianne Wensesla Solang <mariannewencesla@gmail.com>
2022-05-23 20:43:33 +0700

    PSW Proyek_Kelompok 1

--- app/Models/Temuan.php
+++ app/Models/Temuan.php
@@ -8,13 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Temuan extends Model
 {
     use HasFactory;
-    protected $fillable=['nama_barang','slug','no_tel','lokasi','deskripsi','gambar','katagori'];
-
-    public static function getBarangBySlug($slug){
-        // dd($slug);
-        return Temuan::where('slug',$slug)->get();
-    }
-    public static function getAllBarangTemuan(){
-        return Temuan::orderBy('katagori','ASC')->get();
-    }
+    protected $fillable=['nama_barang','slug','no_tel','lokasi','deskripsi','gambar','kategori_id'];
 }
